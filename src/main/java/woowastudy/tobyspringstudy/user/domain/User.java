8358b033ce749d550b6af928ad154dc9526eafb3
TaewanKimmmm <ktww88@gmail.com>
2021-04-26 18:47:06 +0900

    feat: 자바 빈 규약을 따르는 User 클래스 구현

---
+++ src/main/java/woowastudy/tobyspringstudy/user/domain/User.java
@@ -0,0 +1,31 @@
+package woowastudy.tobyspringstudy.user.domain;
+
+public class User { // 자바빈 규약을 따르는 오브젝트
+    String id;
+    String name;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
