fe06ef596d9a058cb9354cad77e82bf65f52a7df
TaewanKimmmm <ktww88@gmail.com>
2021-04-26 17:00:11 +0900

    chore: 환경 설정

---
+++ src/test/java/woowastudy/tobyspringstudy/TobySpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package woowastudy.tobyspringstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
