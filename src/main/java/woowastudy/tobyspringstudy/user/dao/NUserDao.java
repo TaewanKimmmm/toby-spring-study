125d87cd6522647661dab88888cb5f8f529ee072
TaewanKimmmm <ktww88@gmail.com>
2021-04-28 01:04:08 +0900

    refactor: DB 커넥션과 관련된 부분을 별도의 클래스로 분리하고 UserDao가 이용하도록 함

--- src/main/java/woowastudy/tobyspringstudy/user/dao/NUserDao.java
+++ src/main/java/woowastudy/tobyspringstudy/user/dao/NUserDao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class NUserDao extends UserDao{
+public class NUserDao {
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost:13306/toby", "root", "root");
