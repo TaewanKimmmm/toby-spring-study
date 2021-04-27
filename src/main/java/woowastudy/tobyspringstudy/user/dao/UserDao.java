4454e7a37fb702d2c471ad2cb4be65d0ff70f5e5
TaewanKimmmm <ktww88@gmail.com>
2021-04-27 16:16:01 +0900

    refactor: 중복된 DB 연결 코드를 메서드로 추출

--- src/main/java/woowastudy/tobyspringstudy/user/dao/UserDao.java
+++ src/main/java/woowastudy/tobyspringstudy/user/dao/UserDao.java
@@ -6,9 +6,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:13306/toby", "root", "root");
-
+        Connection c = getConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -22,8 +20,8 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:13306/toby", "root", "root");
+
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -40,5 +38,11 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:13306/toby", "root", "root");
+        return c;
     }
 }
