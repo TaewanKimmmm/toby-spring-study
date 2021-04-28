8de3a5597e6c08ef0156c48c1a9cad5ac260c2e2
TaewanKimmmm <ktww88@gmail.com>
2021-04-28 21:09:19 +0900

    refactor: ConnectionMaker 타입 오브젝트 생성 코드 분리하여 중복 제거

--- src/main/java/woowastudy/tobyspringstudy/user/dao/DaoFactory.java
+++ src/main/java/woowastudy/tobyspringstudy/user/dao/DaoFactory.java
@@ -2,8 +2,18 @@ package woowastudy.tobyspringstudy.user.dao;
 
 public class DaoFactory { // ConnectionMaker와 UserDao 오브젝트를 만들고 연결하여 반환한다.
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
+        return new UserDao(connectionMaker());
+    }
+
+    public AccountDao accountDao() {
+        return new AccountDao(connectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(connectionMaker());
+    }
+
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
