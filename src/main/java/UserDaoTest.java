359e3b6a4a77b0d1dc5973d8f6d999ceabc3dc03
TaewanKimmmm <ktww88@gmail.com>
2021-05-06 02:02:15 +0900

    style: 부연 설명 추가

--- src/main/java/UserDaoTest.java
+++ src/main/java/UserDaoTest.java
@@ -9,7 +9,8 @@ public class UserDaoTest {
         // @Configuration 어노테이션이 붙은 자바 코드를 설정정보로 사용하도록 하였다.
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
-        //ApplicationContext 객체의 getBean() 메서드로 UserDao 오브젝트를 가져올 수 있게 되었다.
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        // ApplicationContext 객체의 getBean() 메서드로 UserDao 오브젝트를 가져올 수 있게 되었다.
+        // "userDao" : ApplicationContext에 등록된 빈의 이름
+        UserDao dao = context.getBean("userDao", UserDao.class); // 타입 캐스팅하면서 Object가 아닌 UserDao 객체를 가져오도록 함
     }
 }
--- src/main/java/woowastudy/tobyspringstudy/user/dao/DaoFactory.java
+++ src/main/java/woowastudy/tobyspringstudy/user/dao/DaoFactory.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // Application Context 또는 Bean Factory가 사용할 설정정보라는 표시
 public class DaoFactory { // ConnectionMaker와 UserDao 오브젝트를 만들고 연결하여 반환한다.
     @Bean // 오브젝트 생성을 담당하는 IOC용 메서드라는 표시
+    // userDao라는 메서드명은, 추후에 ApplicationContext에 등록된 빈의 이름이 된다.
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
