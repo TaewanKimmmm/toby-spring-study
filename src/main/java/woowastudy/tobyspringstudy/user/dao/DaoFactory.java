58df28d5eb23fcf754982959cde98a40cf41bf9a
TaewanKimmmm <ktww88@gmail.com>
2021-05-06 01:40:45 +0900

    feat: 스프링이 인식할 수 있도록 Annotation 추가

--- src/main/java/woowastudy/tobyspringstudy/user/dao/DaoFactory.java
+++ src/main/java/woowastudy/tobyspringstudy/user/dao/DaoFactory.java
@@ -1,18 +1,24 @@
 package woowastudy.tobyspringstudy.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // Application Context 또는 Bean Factory가 사용할 설정정보라는 표시
 public class DaoFactory { // ConnectionMaker와 UserDao 오브젝트를 만들고 연결하여 반환한다.
+    @Bean // 오브젝트 생성을 담당하는 IOC용 메서드라는 표시
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
-    public AccountDao accountDao() {
-        return new AccountDao(connectionMaker());
-    }
+//    public AccountDao accountDao() {
+//        return new AccountDao(connectionMaker());
+//    }
+//
+//    public MessageDao messageDao() {
+//        return new MessageDao(connectionMaker());
+//    }
 
-    public MessageDao messageDao() {
-        return new MessageDao(connectionMaker());
-    }
-
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
